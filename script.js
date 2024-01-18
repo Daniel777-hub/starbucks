0c0c0cd2d4f3c25e20d22bf09ad4b60a0bd77cab
Marcus Daniel <127118676+Daniel777-hub@users.noreply.github.com>
2024-01-18 13:43:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let circulo = document.querySelector(".circulo")
 let imgCopo = document.querySelector(".starbucks")
+let verde = document.querySelector(".verde")
+let amarelo = document.querySelector(".amarelo")
+let rosa = document.querySelector(".rosa")
+
+verde.addEventListener ("click", () => {
+    imgCopo.src = "./img/img1.png"
+    circulo.style.background = "#017143"
+})
+
+amarelo.addEventListener ("click", () => {
+    imgCopo.src = "./img/img2.png"
+    circulo.style.background = "#eb7495"
+})
+
+rosa.addEventListener ("click", () => {
+    imgCopo.src = "./img/img3.png"
+    circulo.style.background = "#d752b1"
+})
